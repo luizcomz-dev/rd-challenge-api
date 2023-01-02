bd0f6808393e571537b74dc30235e84143afb203
Luiz Heming <luiz@clinicarx.com.br>
2023-01-02 18:28:01 -0300

    Adicionando as funções do service com texto simples

--- src/launches/launches.service.ts
+++ src/launches/launches.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
 
+/* 
+● Próximo lançamento;
+● Último lançamento;
+● Próximos lançamentos;
+● Lançamentos passados;
+*/
+
 @Injectable()
-export class LaunchesService {}
+export class LaunchesService {
+  getNext() {
+    return 'Get next launch';
+  }
+
+  getLatest() {
+    return 'Get latest launch';
+  }
+
+  getUpcoming() {
+    return 'Get all upcoming launches';
+  }
+
+  getPast() {
+    return 'Get all past launches';
+  }
+}
