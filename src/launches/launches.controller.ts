ddc6377c29172c4deb55ffd9e7bcb0595233f38c
Luiz Heming <luiz@clinicarx.com.br>
2023-01-02 18:28:25 -0300

    Adicionando as rotas no controller consumindo as funções do service

--- src/launches/launches.controller.ts
+++ src/launches/launches.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Version } from '@nestjs/common';
+import { LaunchesService } from './launches.service';
+
+/* 
+● Próximo lançamento;
+● Último lançamento;
+● Próximos lançamentos;
+● Lançamentos passados;
+*/
 
 @Controller('launches')
-export class LaunchesController {}
+export class LaunchesController {
+
+  constructor(private lauchesService: LaunchesService) {}
+
+  @Version('1')
+  @Get('next')
+  getNext() {
+    return this.lauchesService.getNext();
+  }
+
+  @Version('1')
+  @Get('latest')
+  getLatest() {
+    return this.lauchesService.getLatest();
+  }
+
+  @Version('1')
+  @Get('upcoming')
+  getUpcoming() {
+    return this.lauchesService.getUpcoming();
+  }
+
+  @Version('1')
+  @Get('past')
+  getPast() {
+    return this.lauchesService.getPast();
+  }
+
+}
