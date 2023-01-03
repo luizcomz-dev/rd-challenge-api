ea865b34f7212e490a6760c858290996099a1ced
Luiz Heming <luiz@clinicarx.com.br>
2023-01-03 10:19:22 -0300

    Comentando as instruções do arquivo main

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,21 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Pega a configuração do arquivo .env
   const config = app.get(ConfigService);
 
+  // Habilita o versionamento da API
   app.enableVersioning({
     type: VersioningType.URI,
   });
 
+  // Coloca o servidor pra rodar na porta determinada no arquivo .env
   await app.listen(
     config.get('SERVER_PORT') ? parseInt(config.get('SERVER_PORT')) : 3000,
   );
 
+  // Loga no console que o servidor está rodando
   Logger.log(
     `Server running on port ${config.get('SERVER_PORT')}`,
     'Bootstrap',
