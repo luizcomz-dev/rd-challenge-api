65dcc36ce68adb87caf7dd377334a78af09462cf
Luiz Heming <luiz@clinicarx.com.br>
2023-01-03 10:30:49 -0300

    Documentando as requisições com Swagger

--- src/launches/launches.controller.ts
+++ src/launches/launches.controller.ts
@@ -1,38 +1,37 @@
 import { Controller, Get, Version } from '@nestjs/common';
 import { LaunchesService } from './launches.service';
-
-/* 
-● Próximo lançamento;
-● Último lançamento;
-● Próximos lançamentos;
-● Lançamentos passados;
-*/
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('launches')
+@ApiTags('Launches')
 export class LaunchesController {
 
   constructor(private lauchesService: LaunchesService) {}
 
   @Version('1')
   @Get('next')
+  @ApiOperation({ summary: 'Próximo lançamento' })
   getNext() {
     return this.lauchesService.getNext();
   }
 
   @Version('1')
   @Get('latest')
+  @ApiOperation({ summary: 'Último lançamento' })
   getLatest() {
     return this.lauchesService.getLatest();
   }
 
   @Version('1')
   @Get('upcoming')
+  @ApiOperation({ summary: 'Próximos lançamentos' })
   getUpcoming() {
     return this.lauchesService.getUpcoming();
   }
 
   @Version('1')
   @Get('past')
+  @ApiOperation({ summary: 'Últimos lançamentos' })
   getPast() {
     return this.lauchesService.getPast();
   }
